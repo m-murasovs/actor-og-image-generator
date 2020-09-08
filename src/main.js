89721025949647cbc069f7783b526feb5515bee9
Misha <m.murasovs@gmail.com>
2020-09-08 15:39:07 +0200

    Test adding svg logo

--- src/main.js
+++ src/main.js
@@ -52,7 +52,23 @@ Apify.main(async () => {
         
         // Create the elements
         const backgroundContainer = document.createElement('div');
-        
+
+        const logo = document.createElement('span');
+            logo.innerHTML = `<svg width="152px" height="40px" viewBox="0 0 152 40" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <!-- Generator: Sketch 48.1 (47250) - http://www.bohemiancoding.com/sketch -->
+            <title>logo-full-gray</title>
+            <desc>Created with Sketch.</desc>
+            <defs></defs>
+            <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                <g id="logo-full-gray" fill-rule="nonzero">
+                    <path d="M89.597,20.667 L83.92,20.667 L83.92,13.045 L89.599,13.045 C91.697,13.045 93.399,14.752 93.399,16.856 C93.4003269,17.8655873 93.0004736,18.834337 92.2874314,19.5490671 C91.5743892,20.2637971 90.6065876,20.6659398 89.597,20.667 Z M90.171,9 L78.75,9 L78.75,32 L83.919,32 L83.919,24.712 L90.169,24.712 C94.497,24.712 98.005,21.195 98.005,16.856 C98.005,12.517 94.498,9 90.17,9 L90.171,9 Z M110.146,32 L115.313,32 L115.313,23.843 L124.648,23.843 L124.648,19.698 L115.313,19.698 L115.313,13.246 L126.915,13.246 L126.915,9 L110.145,9 L110.145,32 L110.146,32 Z M146.499,9 L140.864,18.46 L140.731,18.46 L135.364,9 L129.496,9 L138.098,23.275 L138.098,32 L143.298,32 L143.298,23.242 L152,9 L146.499,9 Z M101.079,32 L106.247,32 L106.247,9 L101.079,9 L101.079,32 Z M61.269,23.208 L64.469,13.78 L64.602,13.78 L67.736,23.208 L61.268,23.208 L61.269,23.208 Z M61.534,9 L53,32 L58.301,32 L59.901,27.287 L69.103,27.287 L70.703,32 L76.171,32 L67.636,9 L61.535,9 L61.534,9 Z" id="Shape" fill="#212322"></path>
+                    <path d="M5.309,4.755 C1.939,5.187 -0.411,8.042 0.06,11.133 L3.696,35 L19,3 L5.309,4.755 Z" id="Shape" fill="#97D700"></path>
+                    <path d="M39.986,23.133 L38.689,5.145 C38.458,1.947 35.545,-0.39 32.342,0.055 L27,0.794 L38.765,27 C39.6490537,25.9132762 40.0853691,24.5302994 39.985,23.133" id="Shape" fill="#71C5E8"></path>
+                    <path d="M9,39.965 C9.99043651,40.0730706 10.9921479,39.9303619 11.913,39.55 L33,30.886 L22.975,9 L9,39.965 Z" id="Shape" fill="#FF9013"></path>
+                </g>
+            </g>
+        </svg>`;
+
         const imageContainer = document.createElement('div');
         const actorImageCircle = document.createElement('span');
         const actorCoverImg = document.createElement('img');
@@ -80,7 +96,7 @@ Apify.main(async () => {
         
         // Set styles for the elements
         backgroundContainer.setAttribute('style', `
-            background-image: url("https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1280px-Google_2015_logo.svg.png");
+            background: linear-gradient(128deg, rgba(2,0,36,1) 0%, rgba(240,248,254,1) 0%, rgba(255,255,255,1) 100%);
             padding: 0;
             margin: 0;
             background-position: 90% 90%;
@@ -91,6 +107,13 @@ Apify.main(async () => {
             font-family: Graphik-regular, sans-serif;
         `);
         
+        logo.setAttribute('style', `
+            position: absolute;
+            right: 6rem;
+            bottom: 3rem;
+            transform: scale(1.3); 
+        `);
+
         imageContainer.setAttribute('style', `
             width: 100%;
             height: 100%;
@@ -191,7 +214,7 @@ Apify.main(async () => {
         `);
         
         document.body.appendChild(backgroundContainer);
-        backgroundContainer.append(imageContainer, descriptionContainer);
+        backgroundContainer.append(imageContainer, descriptionContainer, logo);
         descriptionContainer.append(titleContainer, authorInfoContainer, tryButtonContainer);
         titleContainer.append(titleText);
         imageContainer.append(actorImageCircle);
